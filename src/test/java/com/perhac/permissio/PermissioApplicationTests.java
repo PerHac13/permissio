d398e0ba04a705f94c9b13fb19b98dac3f9b89e4
PerHac13 <abdullah13112004@gmail.com>
2026-08-14 16:23:32 +0530

    test: first ever test
    -based on the active profiles config

--- src/test/java/com/perhac/permissio/PermissioApplicationTests.java
+++ src/test/java/com/perhac/permissio/PermissioApplicationTests.java
@@ -2,8 +2,10 @@ package com.perhac.permissio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PermissioApplicationTests {
 
 	@Test
@@ -11,3 +13,4 @@ class PermissioApplicationTests {
 	}
 
 }
+
