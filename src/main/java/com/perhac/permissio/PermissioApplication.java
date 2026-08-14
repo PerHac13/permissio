5d15c05ef671c0ca5751471af8f322e3896b28ae
PerHac13 <abdullah13112004@gmail.com>
2026-08-14 14:00:07 +0530

    chore!: initial startup setup kit

---
+++ src/main/java/com/perhac/permissio/PermissioApplication.java
@@ -0,0 +1,13 @@
+package com.perhac.permissio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perhac/permissio/PermissioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perhac.permissio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
