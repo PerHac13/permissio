fa117dd0f7b0f71389e092fa0fdebf50daed223a
PerHac13 <abdullah13112004@gmail.com>
2026-08-17 21:30:32 +0530

    chore: action enums

---
+++ src/main/java/com/perhac/permissio/common/model/Action.java
@@ -0,0 +1,13 @@
+package com.perhac.permissio.common.model;
+
+/**
+ * Universal permission actions supported in Permissio ReBAC policies.
+ */
+public enum Action {
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    APPROVE,
+    REJECT
+}
